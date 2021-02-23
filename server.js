9610e7f5fcb1003979070aa89818833a5998a282
Vavyo <vavyo2121@gmail.com>
2021-02-23 15:11:28 -0600

    book constructor and search post

--- server.js
+++ server.js
@@ -16,9 +16,23 @@ app.use(express.static('public'));
 
 // temporary routes
 app.get('/hello', displayIndex);
+app.post('/searches', searchBooks);
 
 function displayIndex(req, res) {
   res.render('pages/index');
+}
+
+function searchBooks(req, res){
+  const searchTerm = req.body.term;
+  console.log(req.body);
+}
+
+// book constructor
+function Book(_title, _author, _thumbnail, _link){
+  this.title = _title;
+  this.author = _author?_author:'Author Unknown';
+  this.thumbnail = _thumbnail?_thumbnail:'https://i.imgur.com/J5LVHEL.jpg';
+  this.link = _link;
 }
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
