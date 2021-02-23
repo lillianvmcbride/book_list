e773c61d5ea02bd38af5121e0a33aaa3696e1fbc
Vavyo <vavyo2121@gmail.com>
2021-02-23 17:35:08 -0600

    Card 3 almost done

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 // set view engine and supply public files.
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-
+app.use(express.urlencoded({extended: true}));
 
 // temporary routes
 app.get('/hello', displayIndex);
@@ -28,16 +28,33 @@ function newSearch(req, res){
 }
 
 function searchBooks(req, res){
-  const searchTerm = req.body.term;
-  console.log(req.body);
+  // console.log('req body', req.body);
+  const searchQuery = req.body.searchQuery;
+  // console.log('🐱‍👤',searchQuery);
+  const searchType = req.body.searchType;
+  // console.log('🍱',searchType);
+  const url = `https://www.googleapis.com/books/v1/volumes?q=+in${searchType}:${searchQuery}`;
+
+  superagent(url)
+    .then((results) => results.body.items.map(bookResults => new Book(bookResults.volumeInfo)))
+    .then(results => res.render('pages/searches/show.ejs',{results: results}));
+  // .then(library => {
+  //   const mapBooks = library.body.items.map(books => )
+  //   console.log ('📚',books.body.items);
+  // });
+  // res.render('pages/searches/show');
 }
 
 // book constructor
-function Book(_title, _author, _thumbnail, _link){
-  this.title = _title;
-  this.author = _author?_author:'Author Unknown';
-  this.thumbnail = _thumbnail?_thumbnail:'https://i.imgur.com/J5LVHEL.jpg';
-  this.link = _link;
+function Book(bookObject){
+  this.title = bookObject.title;
+  this.author = bookObject.authors?bookObject.authors[0]:'Author Unknown';
+  // console.log('🍙', bookObject.imageLinks);
+  // console.log('🍙🍱', bookObject.imageLinks.thumbnail?true:false);
+  this.thumbnail = bookObject.imageLinks?(bookObject.imageLinks.thumbnail?bookObject.imageLinks.thumbnail:'https://i.imgur.com/J5LVHEL.jpg'):'https://i.imgur.com/J5LVHEL.jpg';
+  this.description = bookObject.description;
+  this.link = bookObject.infoLink;
+  // console.log(this);
 }
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
